fcb8d7f77778beca977cfddd4954dab2ffc01d28
naoallro <145730570+naoallro@users.noreply.github.com>
2025-04-14 23:18:03 -0600

    Semana11

--- src/main/java/com/tienda/controller/IndexController.java
+++ src/main/java/com/tienda/controller/IndexController.java
@@ -1,6 +1,7 @@
 
 package com.tienda.controller;
 
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,8 +14,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class IndexController {
 
     @RequestMapping("/")
-    public String principal(Model model) {
+    public String principal(Model model, HttpSession session) {
         //model.addAttribute("attribute", "value");
+        Long idUsuario = (Long)session.getAttribute("idUsuario");
         return "index";
     }
 //    @RequestMapping("/informacion")
